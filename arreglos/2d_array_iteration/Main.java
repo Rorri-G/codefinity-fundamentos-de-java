54e83030f91cd43a8a15755ff437118278b11bef
Rorri-G <>
2025-11-08 13:59:50 +0000

    2D Array Iteration

--- arreglos/2d_array_iteration/Main.java
+++ arreglos/2d_array_iteration/Main.java
@@ -1,3 +1,22 @@
+/*
+Calcular el promedio de temperatura para cada día de la semana utilizando datos de varias estaciones.
+
+Se proporcionan los siguientes datos:
+
+Estación 1: {20, 22, 24, 19, 21, 23, 25}
+Estación 2: {18, 20, 22, 19, 20, 22, 24}
+Estación 3: {21, 23, 25, 22, 24, 26, 28}
+Estación 4: {19, 21, 23, 20, 22, 24, 26}
+Tarea:
+Escribir un método llamado calculateDailyAverages(int[][] temperatures) que devuelva un double[].
+En este método:
+Recorrer cada día (columnas del arreglo bidimensional).
+Para cada día, inicializar totalTemp para almacenar la suma de las temperaturas.
+Utilizar un ciclo interno para recorrer todas las estaciones (filas).
+Sumar las temperaturas de cada estación para el día actual.
+
+
+*/
 package com.example;
 
 public class Main {
